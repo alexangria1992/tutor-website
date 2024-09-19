a84d7a5b9b0424cd29e099be80c05dd9d2572d7c
Alexandre Angrignon <alex040892@gmail.com>
2024-09-19 18:41:29 +1200

    desktop hero Section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
+  const toggleButton = document.querySelector(' .mobile-menu-toggle');
   //   console.log(toggleButton);
-  const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
+  const mobileMenu = document.querySelector(' .mobile-menu-items');
 
   toggleButton.addEventListener('click', function () {
     mobileMenu.classList.toggle('active');
